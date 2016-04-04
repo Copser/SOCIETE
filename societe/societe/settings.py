bb1bb725ed4dc5b652d2180932f135aa0b6b4b4f
Copser <iamrmot@gmail.com>
2016-04-04 21:08:48 +0200

    debug=True

--- societe/societe/settings.py
+++ societe/societe/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j-%*5#5wtg9#128lvye-ln8^-s6__3&&*jm-_s)&h_2hj61fs_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
