0f7bf1d7ff1d5f282f776155a94d70304c7a588a
Copser <iamrmot@gmail.com>
2016-06-04 06:42:23 +0200

    adding stripe

---
+++ societe/static/bower_components/bootstrap/dist/js/tokenization.js
@@ -0,0 +1,32 @@
+    var stripeResponseHandler = function(status, response) {
+            var $form = $('#payment-form');
+
+            if (response.error) {
+                // Show the errors on the form
+                $form.find('.payment-errors').text(response.error.message);
+                $form.find('.payment-errors').addClass('alert');
+                $form.find('.payment-errors').addClass('alert-error');
+                $form.find('button').prop('disabled', false);
+            } else {
+                // token contains id, last4, and card type
+                var token = response.id;
+                // Insert the token into the form so it gets submitted to the server
+                $form.append($('<input type="hidden" name="stripe_token" />').val(token));
+                // and re-submit
+                $form.get(0).submit();
+            }
+        };
+
+        $(function($) {
+            $('#payment-form').submit(function(e) {
+                var $form = $(this);
+
+                // Disable the submit button to prevent repeated clicks
+                $form.find('button').prop('disabled', true);
+
+                Stripe.createToken($form, stripeResponseHandler);
+
+                // Prevent the form from submitting with the default action
+                return false;
+            });
+        });
