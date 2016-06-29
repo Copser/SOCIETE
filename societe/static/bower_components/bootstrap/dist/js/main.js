5503a53347f78708385abf770c61e5b6e3d3473b
Copser <iamrmot@gmail.com>
2016-06-29 06:50:34 +0200

    mocking new ladning page

--- societe/static/bower_components/bootstrap/dist/js/main.js
+++ societe/static/bower_components/bootstrap/dist/js/main.js
@@ -1,4 +1,21 @@
+function checkScroll(){
+    var startY = $('.navbar').height() * 2;
+
+    if ($(window).scrollTop() > startY) {
+         $('.navbar').addClass("scrolled");
+    } else {
+         $('.navbar').removeClass("scrolled");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
+
 $(document).ready(function(){
+
     $("#showlogin").click(function(){
         $("#loginModal").modal('show');
     });
