aa82e338f55eb45578f98f66cdc9a4bc972201a4
Copser <iamrmot@gmail.com>
2016-07-12 22:13:36 +0200

    routing thank_you.html

--- societe/societe/urls.py
+++ societe/societe/urls.py
@@ -20,10 +20,8 @@ urlpatterns = [
     # paypal payment notify url
     # url(r'^paypal/', include('paypal.standard.ipn.urls')),
     # stripe authentication payment urls
-    url(r'^sign_in/', views.sign_in, name='sign_in'),
-    url(r'^sign_out/', views.sign_out, name='sign_out'),
     url(r'^register/', views.register, name='register'),
-    url(r'^edit/', views.edit, name='edit'),
+    url(r'^thank_you/', views.thank_you, name='thank_you'),
 
     # allauth urls
     # url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
