226fc59c357dfa7f6d1cc19d1e1860551cfd9bf6
Copser <iamrmot@gmail.com>
2016-07-14 21:37:02 +0200

    memcache configuration

--- societe/societe/wsgi.py
+++ societe/societe/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
+from django.core.cache.backends.memcached import BaseMemcachedCache
 # import os
 
 # from django.core.wsgi import get_wsgi_application
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "societe.settings")
-
+BaseMemcachedCache.close = lambda self, **kwargs: None
 application = Cling(get_wsgi_application())
