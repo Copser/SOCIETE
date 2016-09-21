ec4620a268f7b47fa1d0425c1fdec3e6548228bf
Copser <ppilipovic84@gmail.com>
2016-09-21 21:32:01 +0200

    fix typo

--- superjobs/superjobs/settings.py
+++ superjobs/superjobs/settings.py
@@ -90,7 +90,7 @@ AUTHENTICATION_BACKENDS = (
 WSGI_APPLICATION = 'superjobs.wsgi.application'
 
 # this also use allauth
-site_id = 3
+SITE_ID = 2
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
