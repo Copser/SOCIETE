2939bf7cf015c314cf53361c1daefe9775ff11d6
Copser <ppilipovic84@gmail.com>
2016-09-21 20:21:46 +0200

    change site_id to live name

--- superjobs/superjobs/settings.py
+++ superjobs/superjobs/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -50,7 +50,7 @@ INSTALLED_APPS = (
     'bootstrap3',
 )
 
-SITE_ID = 2
+SITE_ID = 'https://supejobs.herokuapp.com'
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
