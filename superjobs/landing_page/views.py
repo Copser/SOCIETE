05fb6ef846d1d347cbd48b2cbd0e4e1be10e0fba
Copser <ppilipovic84@gmail.com>
2016-10-26 21:00:29 +0200

    seting up simple view for landing page and about page

--- superjobs/landing_page/views.py
+++ superjobs/landing_page/views.py
@@ -3,10 +3,19 @@ from django.template import RequestContext
 
 # Create your views here.
 def index(request):
-    """TODO: Landing Page
+    """TODO: Setup simple view to render landing page
     return: TODO
     """
     return render_to_response(
         'index.html',
+        context_instance=RequestContext(request)
+    )
+
+def about(request):
+    """TODO: Setup simple view to render about page
+    return: TODO
+    """
+    return render_to_response(
+        'about.html',
         context_instance=RequestContext(request)
     )
