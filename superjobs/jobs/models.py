4bc4a3e091b3c0ef0ca401e994e4b22e07101fb8
Copser <ppilipovic84@gmail.com>
2016-10-27 06:34:01 +0200

    setup new jobs app, for are jobs ad, init models.py and register are models into admin

---
+++ superjobs/jobs/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class CarpenterAd(models.Model):
+    """TODO: init CarpenterAd, we will register this model into Admin
+    return: TODO
+    """
+    pass
+
+
+class HousekeepAd(models.Model):
+    """TODO: init CarpenterAd, we will register this model into Admin
+    return: TODO
+    """
+    pass
+
+
+class PlumbingAd(models.Model):
+    """TODO: init CarpenterAd, we will register this model into Admin
+    return: TODO
+    """
+    pass
+
+
+class ElectricalAd(models.Model):
+    """TODO: init CarpenterAd, we will register this model into Admin
+    return: TODO
+    """
+    pass
+
+
+class ConstructionAd(models.Model):
+    """TODO: init CarpenterAd, we will register this model into Admin
+    return: TODO
+    """
+    pass
+
+
+class HAVCAd(models.Model):
+    """TODO: init CarpenterAd, we will register this model into Admin
+    return: TODO
+    """
+    pass
