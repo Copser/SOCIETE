e83156e1ee7cdd66176364acd8b57ce8170d3832
Copser <ppilipovic84@gmail.com>
2016-11-08 06:35:49 +0100

    write simple test case for are models

--- superjobs/jobs/tests.py
+++ superjobs/jobs/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from jobs.models import Job
 
 # Create your tests here.
+class JobTests(TestCase):
+    """TODO: writing small db unittests for Job model
+    return: TODO
+    """
+    def test_str(self):
+        jobs_title = Job(
+            job_title="Title for a Job"
+        )
+        self.assertEquals(
+            str(jobs_title), 'Title for a Job'
+        )
