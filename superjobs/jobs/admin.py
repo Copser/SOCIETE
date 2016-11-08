51e1b90bf016630d96921c45a98322b9c4029539
Copser <ppilipovic84@gmail.com>
2016-11-08 06:07:24 +0100

    fix typo

--- superjobs/jobs/admin.py
+++ superjobs/jobs/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import Job, ApplyToJob
+
 # Register your models here.
+class JobAdmin(admin.ModelAdmin):
+    """TODO: register model into admin with a small customization for the model, we are including
+    some fieldsets, so we will separate jobs categories from jobs title, description and created_at field
+    return: TODO
+    """
+    fieldsets = [
+        ('Job General Information', {'fields': ['job_title',
+                                                 'job_description',
+                                                 'job_created_at',],
+                                      'classes': ['collapse']}),
+        ('Job Lists', {'fields': ['job_categories']}),]
+
+
+
+class ApplyToJobAdmin(admin.ModelAdmin):
+    """TODO: register apply_to form
+    return: TODO
+    """
+    model = ApplyToJob
+
+
+admin.site.register(Job, JobAdmin)
+admin.site.register(ApplyToJob, ApplyToJobAdmin)
