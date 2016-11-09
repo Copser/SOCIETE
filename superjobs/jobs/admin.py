a509c6e00e32c737ac5d4a44922c867f864df55b
Copser <ppilipovic84@gmail.com>
2016-11-09 20:23:27 +0100

    rename imported model in admin

--- superjobs/jobs/admin.py
+++ superjobs/jobs/admin.py
@@ -1,28 +1,28 @@
 from django.contrib import admin
 
-from .models import Job, ApplyToJob
+from .models import Jobs, ApplyToJobs
 
 # Register your models here.
-class JobAdmin(admin.ModelAdmin):
+class JobsAdmin(admin.ModelAdmin):
     """TODO: register model into admin with a small customization for the model, we are including
     some fieldsets, so we will separate jobs categories from jobs title, description and created_at field
     return: TODO
     """
     fieldsets = [
-        ('Job General Information', {'fields': ['job_title',
-                                                 'job_description',
-                                                 'job_created_at',],
+        ('Job General Information', {'fields': ['jobs_title',
+                                                 'jobs_description',
+                                                 'jobs_created_at',],
                                       'classes': ['collapse']}),
-        ('Job Lists', {'fields': ['job_categories']}),]
+        ('Job Lists', {'fields': ['jobs_categories']}),]
 
 
 
-class ApplyToJobAdmin(admin.ModelAdmin):
+class ApplyToJobsAdmin(admin.ModelAdmin):
     """TODO: register apply_to form
     return: TODO
     """
-    model = ApplyToJob
+    model = ApplyToJobs
 
 
-admin.site.register(Job, JobAdmin)
-admin.site.register(ApplyToJob, ApplyToJobAdmin)
+admin.site.register(Jobs, JobsAdmin)
+admin.site.register(ApplyToJobs, ApplyToJobsAdmin)
