fc283eaec1d0fa52984d6fbd3ce85a474374fedb
Copser <ppilipovic84@gmail.com>
2016-11-09 22:13:55 +0100

    creating view to list are jobs from db

--- superjobs/blog/views.py
+++ superjobs/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import Context, loader
 
@@ -10,6 +10,31 @@ def jobs(request):
     return: TODO
     """
     latest_posts = Post.objects.all().order_by('-created_at')
-    t = loader.get_template('blog/jobs.html')
-    c = Context({'latest_posts': latest_posts, })
-    return HttpResponse(t.render(c))
+    t = loader.get_template(
+        'blog/jobs.html'
+    )
+    c = Context({
+        'latest_posts': latest_posts,
+    })
+    return HttpResponse(
+        t.render(c)
+    )
+
+
+def post(request, post_id):
+    """TODO: creating post so we can sort out are jobs list
+    return: TODO
+    """
+    single_post = get_object_or_404(
+        Post,
+        pk=post_id
+    )
+    t = loader.get_template(
+        'blog/post.html'
+    )
+    c = Context({
+        'single_post': single_post,
+    })
+    return HttpResponse(
+        t.render(c)
+    )
