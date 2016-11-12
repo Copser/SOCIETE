2cff389074678a69b59b9c69620c357ff14cc605
Copser <ppilipovic84@gmail.com>
2016-11-12 20:56:38 +0100

    make success router

--- superjobs/blog/urls.py
+++ superjobs/blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^jobs/$', views.jobs, name='jobs'),
     url(r'^(?P<slug>[\w|\-]+)/$', views.post, name='post'),
     url(r'^apply_to/$', views.apply_to, name='apply_to'),
+    url(r'^success/$', views.success, name='success'),
 
 ]
