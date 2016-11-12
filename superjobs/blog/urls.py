b2dbeadf1032b5810c236bc115745c307f2bac37
Copser <ppilipovic84@gmail.com>
2016-11-12 20:33:41 +0100

    change .url with slug, so we can support it

--- superjobs/blog/urls.py
+++ superjobs/blog/urls.py
@@ -5,7 +5,7 @@ from blog import views
 urlpatterns = [
 
     url(r'^jobs/$', views.jobs, name='jobs'),
-    url(r'^(?P<post_url>\w+)/$', views.post, name='post'),
+    url(r'^(?P<slug>[\w|\-]/$', views.post, name='post'),
     url(r'^apply_to/$', views.apply_to, name='apply_to'),
 
 ]
