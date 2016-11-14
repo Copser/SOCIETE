e69d06a46c660045396d4cdd679fd3e27478fe07
Copser <ppilipovic84@gmail.com>
2016-11-14 19:12:16 +0100

    prepare app for heroku

--- superjobs/superjobs/settings.py
+++ superjobs/superjobs/settings.py
@@ -15,14 +15,14 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -107,9 +107,9 @@ WSGI_APPLICATION = 'superjobs.wsgi.application'
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # this SITE_ID we are using in local development
-SITE_ID = 4
+# SITE_ID = 4
 # After research I've manually added SITE_ID on heroku and I'm using SITE_ID=2 in production
-# SITE_ID = 2
+SITE_ID = 2
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
@@ -140,10 +140,10 @@ USE_TZ = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Heroku Configurations
 # Parse database configuration from $DATABASE_URL
