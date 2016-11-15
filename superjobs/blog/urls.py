e2f12961fd5f76261f507d161f506dcc2d3c01de
Copser <ppilipovic84@gmail.com>
2016-11-15 22:41:38 +0100

    fix typo

--- superjobs/blog/urls.py
+++ superjobs/blog/urls.py
@@ -5,13 +5,13 @@ from blog import views
 
 urlpatterns = [
 
+    url(r'^posts/$', views.posts_list),
+    url(r'^posts/(?P<pk>[0-9]+)/$', views.posts_detail),
+
     url(r'^jobs/$', views.jobs, name='jobs'),
     url(r'^apply_to/$', views.apply_to, name='apply_to'),
     url(r'^success/$', views.success, name='success'),
     url(r'^(?P<slug>[\w|\-]+)/$', views.post, name='post'),
-
-    url(r'^posts/$', views.post_list),
-    url(r'^posts/(?P<pk>[0-9]+)/$', views.post_detail),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
