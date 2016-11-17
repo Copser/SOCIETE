0e7b496905fe2f4f8d16076e42028a3b04b5fd82
Copser <ppilipovic84@gmail.com>
2016-11-17 05:47:19 +0100

    fix typo;

--- superjobs/blog/urls.py
+++ superjobs/blog/urls.py
@@ -6,7 +6,7 @@ from blog import views
 urlpatterns = [
 
     url(r'^posts/$', views.posts_list),
-    url(r'^posts/(?p<pk>[0-9]+)/$', views.posts_detail),
+    url(r'^posts/(?P<pk>[0-9]+)/$', views.posts_detail),
 
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
