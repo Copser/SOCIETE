522fdc6edb9e241b8ba258b6117ed5a374deb112
Copser <ppilipovic84@gmail.com>
2016-11-28 21:12:18 +0100

    register field in form

--- superjobs/blog/forms.py
+++ superjobs/blog/forms.py
@@ -13,12 +13,16 @@ class ApplyForm(forms.ModelForm):
             years=range(1975, 2010)
         )
     )
+    applicants_cv = forms.FileField(
+        label="Upload you're resume",
+        help_text = 'max. 25 megabytes'
+    )
     class Meta:
         model = Apply
         fields = ['full_name', 'email', 'mobile', 'birthdate',
                   'previous_company_name', 'previous_company_email', 'previous_job_title',
                   'jobs_experience', 'hospitality_relations_experience',
-                  'working_hours', 'choose_desired_working_hours_wage',
+                  'working_hours', 'choose_desired_working_hours_wage', 'applicants_cv',
                   'type_of_driver_licences']
         widgets = {
             'choose_desired_working_hours_wage': forms.NumberInput(attrs= {'min': '0', 'max': '35', 'step': '1'}),
