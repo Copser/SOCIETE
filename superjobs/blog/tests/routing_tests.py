6f8e547b65337f2b2a2ac922cdba4ca4bd0aceff
Copser <ppilipovic84@gmail.com>
2016-12-01 20:29:27 +0100

    remove old tests starting new with pytest

--- superjobs/blog/tests/routing_tests.py
+++ superjobs/blog/tests/routing_tests.py
@@ -1,7 +1,9 @@
+import pytest
+
+from django.conf import settings
 from django.test import TestCase
 from django.core.urlresolvers import resolve, reverse
 from django.shortcuts import render_to_response
-from blog.views import jobs
 
 
 class JobsPageTest(TestCase):
@@ -13,24 +15,5 @@ class JobsPageTest(TestCase):
         """TODO: should return jobs page
         return: TODO
         """
-        jobs_page = resolve('/blog/jobs')
-        self.assertEqueal(jobs_page.func, jobs)
-
-    def test_valid_status_code(self):
-        """TODO: return status code, and verify jobs html page
-        return: TODO
-        """
-        jobs = self.client.get('/blog/jobs')
-        self.assertEqueals(jobs.status_code, 200)
-        self.asertTemplateUser(jobs, "jobs.html")
-
-    def test_returns_exact_html(self):
-        """TODO: expend templates test so it can confirme are we
-        using proper template
-        return: TODO
-        """
-        jobs = self.client.get('/blog/jobs')
-        self.assertEqueal(
-            jobs.content,
-            render_to_response("jobs.html").content
-        )
+        jobs_page = resolve('/')
+        assert self.jobs_page
