5908a0dbf1ef1ef34fc146ffacfc3e3a3f529179
Copser <ppilipovic84@gmail.com>
2016-12-01 22:09:17 +0100

    status code test passed

--- superjobs/landing_page/tests/routing_tests.py
+++ superjobs/landing_page/tests/routing_tests.py
@@ -12,15 +12,29 @@ class MainPageTest(TestCase):
     """
 
     def test_root_resolvers_to_main_view(self):
+        """TODO: test main page url routing
+        retun: TODO
+        """
         main_page = resolve('/')
         assert main_page.func, index
 
     def test_root_resolvers_to_about_view(self):
+        """TODO: test about page url routing
+        return: TODO
+        """
         about_page = resolve('/about/')
         assert about_page.func, about
 
-    def test_returns_appropriate_html(self):
+    def test_returns_appropriate_main_page_status_code(self):
+        """TODO: test main page return proper status code
+        return: TODO
+        """
         index = self.client.get('/')
-        about = self.client.get('/about')
         assert index.status_code, 200
+
+    def test_returns_appropriate_about_page_status_code(self):
+        """TODO: test about page return proper status code
+        return: TODO
+        """
+        about = self.client.get('/about')
         assert about.status_code, 200
