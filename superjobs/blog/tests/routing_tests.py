e37f6e79202783b43df92ddf91cd2a3f3b455ebc
Copser <ppilipovic84@gmail.com>
2016-12-01 21:52:53 +0100

    testing blog/views.py routing

--- superjobs/blog/tests/routing_tests.py
+++ superjobs/blog/tests/routing_tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve, reverse
 from django.shortcuts import render_to_response
 
+from blog.views import jobs
 
 class JobsPageTest(TestCase):
     """TODO: testing blog/ routing, we want to see if are pages are
@@ -14,5 +15,5 @@ class JobsPageTest(TestCase):
         """TODO: should return jobs page
         return: TODO
         """
-        jobs_page = resolve('/')
-        assert self.jobs_page
+        jobs_page = resolve('/blog/jobs')
+        assert jobs_page.func, jobs
