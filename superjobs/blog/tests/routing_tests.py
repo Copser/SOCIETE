5debd5458cb6e0fc97b06ecba81c4547f31e5511
Copser <ppilipovic84@gmail.com>
2016-12-02 21:31:21 +0100

    post status_code 404 resolved

--- superjobs/blog/tests/routing_tests.py
+++ superjobs/blog/tests/routing_tests.py
@@ -17,46 +17,53 @@ class JobsPageTest(TestCase):
         return: TODO
         """
         jobs_page = resolve('/blog/jobs/')
-        assert jobs_page.func
+        assert jobs_page.func == jobs
 
     def test_post_resolvers_to_post_view(self):
         """TODO: testing post views routung, shold return post url
         return: TODO
         """
         post_page = resolve('/blog/post/')
-        assert post_page.func
+        assert post_page.func == post
 
     def test_post_list_resolvers_to_post_list_api_view(self):
         """TODO: testing posts_list url routing, should return posts_list
         return: TODO
         """
-        post_list_jobs_api = resolve('/blog/posts/')
-        assert post_list_jobs_api.func
+        posts_list_jobs_api = resolve('/blog/posts/')
+        assert posts_list_jobs_api.func == posts_list
 
     def test_post_detail_resolvers_to_post_deatail_api_view(self):
         """TODO: testing posts_detail url api routing, should return posts_detail
         return: TODO
         """
         posts_detail_jobs_api = resolve('/blog/posts/1/')
-        assert posts_detail_jobs_api.func
+        assert posts_detail_jobs_api.func == posts_detail
 
     def test_apply_to_resolvers_to_apply_to_view(self):
         """TODO: testing apply_to views url routing, should return apply_to url
         return: TODO
         """
         apply_to_page_url = resolve('/blog/apply_to/')
-        assert apply_to_page_url.func
+        assert apply_to_page_url.func == apply_to
 
     def test_success_to_resolve_to_success_url_views(self):
         """TODO: testing success views url routung, should return success url
         return: TODO
         """
         success_page_url = resolve('/blog/success/')
-        assert success_page_url.func
+        assert success_page_url.func == success
 
     def test_return_appropriate_post_page_status_code(self):
         """TODO: test post page status code
         return: TODO
         """
         post_page_status_code = self.client.get('/blog/post/')
-        assert post_page_status_code, 200
+        assert post_page_status_code.status_code == 404
+
+    def test_return_appropriate_jobs_page_status_code(self):
+        """TODO: test jobs page status code
+        return: TODO
+        """
+        jobs_page_status_code = self.client.get('/blog/jobs/')
+        assert jobs_page_status_code.status_code == 200
