b36de842be20b2c07bd3309cddf41aab3da42e83
Copser <ppilipovic84@gmail.com>
2016-12-02 20:14:53 +0100

    testing blog/views routing

--- superjobs/blog/tests/routing_tests.py
+++ superjobs/blog/tests/routing_tests.py
@@ -1,10 +1,11 @@
 import pytest
-
 from django.test import TestCase
+
 from django.core.urlresolvers import resolve, reverse
 from django.shortcuts import render_to_response
 
-from blog.views import jobs
+from blog.views import jobs, post, posts_list, posts_detail, \
+        apply_to, success
 
 class JobsPageTest(TestCase):
     """TODO: testing blog/ routing, we want to see if are pages are
@@ -15,5 +16,19 @@ class JobsPageTest(TestCase):
         """TODO: should return jobs page
         return: TODO
         """
-        jobs_page = resolve('/blog/jobs')
+        jobs_page = resolve('/blog/jobs/')
         assert jobs_page.func, jobs
+
+    def test_post_resolvers_to_post_view(self):
+        """TODO: testing post views routung, shold return post url
+        return: TODO
+        """
+        post_page = resolve('/blog/post/')
+        assert post_page.func, post
+
+    def test_post_list_resolvers_to_post_list_api_view(self):
+        """TODO: testing posts_list url routing, should return posts_list
+        return: TODO
+        """
+        post_list_jobs_api = resolve('/blog/posts/')
+        assert post_list_jobs_api.func, post
