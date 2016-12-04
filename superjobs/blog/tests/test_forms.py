3eebcd6881a0724a3bb57b9992d6ead1caac39ee
Copser <ppilipovic84@gmail.com>
2016-12-04 06:57:52 +0100

    preapre for form_testing

--- superjobs/blog/tests/test_forms.py
+++ superjobs/blog/tests/test_forms.py
@@ -1,28 +1,9 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
-class FormTestMixin():
-    """TODO: Create a mixin which will validate are form, we pass this into
-    class of the form, name of the field expected to have and error, the
-    expected error message, and the ata to initialize the form. This mixin
-    will to appropriate validation and provide a helpful error message
+from blog.forms import ApplyForm
+
+class FormTestCase():
+    """TODO: Testing apply_to form
     return: TODO
     """
-    def assertFormError(self, form_cls,
-                        expected_error_name,
-                        expected_error_msg,
-                        data):
-        from pprint import pformat
-        test_form = form_cls(data=data)
-
-        # if we got and error the the form should not be valid
-        self.assertFalse(test_form.is_valid())
-
-        self.assertEqueals(
-            test_form.errors[expected_error_name],
-            expected_error_msg,
-            msg="Expected {}: Actual {} : using data {}".format(
-                test_form.errors[expected_error_name],
-                expected_error_msg, pformat(data)
-            )
-        )
+    pass
