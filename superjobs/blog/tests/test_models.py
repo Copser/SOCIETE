5d28787063b4f5452b29e88e25372ba5eb833e1b
Copser <ppilipovic84@gmail.com>
2016-12-04 07:06:46 +0100

    assert error in slug field

--- superjobs/blog/tests/test_models.py
+++ superjobs/blog/tests/test_models.py
@@ -16,9 +16,13 @@ class TestCaseModel():
         """TODO: testing post field, will be create correctly
         return: TODO
         """
-        post_jobs = Post(title="TestJob", description="We need you!", tag="plumbing, housekeep")
+        post_jobs = Post(title="TestJob",
+                         description="We need you!",
+                         tag="plumbing, housekeep",
+                        slug="test-job")
         post_jobs.save()
         assert post_jobs.title == "TestJob"
         assert post_jobs.description == "We need you!"
         assert post_jobs.tag == "plumbing, housekeep"
+        assert post_jobs.slug == "test-job"
 
