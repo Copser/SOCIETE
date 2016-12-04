a32fdf27be0f482a8464dcf15184614e78bb6373
Copser <ppilipovic84@gmail.com>
2016-12-04 21:48:08 +0100

    writing form validation tests

--- superjobs/blog/tests/test_forms.py
+++ superjobs/blog/tests/test_forms.py
@@ -1,14 +1,27 @@
 import pytest
+from django import forms
 
 from blog.forms import ApplyForm
 
-class FormTestCase():
-    """TODO: Testing apply_to form
+def test_apply_to_form_data_validation_for_invalid_data(self):
+    """TODO: testing data validation in ApplyForm
     return: TODO
     """
-    def test_form_data_validation_for_invalida_data(self):
-        """TODO: testing form validation, if data is true it passes,
-        but if not it will return validation error
-        return: TODO
-        """
-        pass
+    form = ApplyForm(
+        {
+            'full_name': 'John Doe',
+            'email': 'johndoe@gmail.com',
+            'mobile': '',
+            'birthdate': '10.14.1984',
+            'previous_company_name': 'Company Name',
+            'previous_company_email': 'company@example.com',
+            'previous_job_title': 'mad hatter',
+            'jobs_experience': 'I have it',
+            'hospitality_relations_experience': 'I have this to',
+            'working_hours': '200',
+            'choose_desired_working_hours_wage': '10$',
+            'type_of_driver_licences': 'A'}
+    )
+
+    assert form.is_valid() == True
+
