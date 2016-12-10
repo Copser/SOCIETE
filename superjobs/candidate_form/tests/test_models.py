b92bea6a31daa817c10411a8b47644bdd35fa042
Copser <ppilipovic84@gmail.com>
2016-12-10 21:43:33 +0100

    decimal field max_digits was increased

--- superjobs/candidate_form/tests/test_models.py
+++ superjobs/candidate_form/tests/test_models.py
@@ -9,20 +9,53 @@ import pytest
 
 
 @pytest.mark.django_db
-def test_personal_information_models_fields():
-    personal_info = CandindateFormModel(
+def test_candidate_form_models_field():
+    candidate_info = CandindateFormModel(
+        #
+        # Personal Information
+        #
         first_name="John",
         last_name="Doe",
         email="johndoe@gmail.com",
         mobile_phone_number="3876543434343",
         city="New York",
-        street_address="Broadway"
+        street_address="Broadway",
+        #
+        # Experience Information
+        #
+        candidate_skill="Housekeep",
+        candidate_experience="One Year +",
+        candidate_hospitality_experience="Two Year",
+        candidate_training="Self Taught",
+        #
+        # Additional Information
+        # 
+        work_hours="30+",
+        payed_per_hour="20.0",
+        valid_work_permit="Yes",
+        drivers_license="C"
     )
-    personal_info.save()
-
-    assert personal_info.first_name == "John"
-    assert personal_info.last_name == "Doe"
-    assert personal_info.email == "johndoe@gmail.com"
-    assert personal_info.mobile_phone_number == "3876543434343"
-    assert personal_info.city == "New York"
-    assert street_address == "Broadway"
+    candidate_info.save()
+    #
+    # Personal Information
+    #
+    assert candidate_info.first_name == "John"
+    assert candidate_info.last_name == "Doe"
+    assert candidate_info.email == "johndoe@gmail.com"
+    assert candidate_info.mobile_phone_number == "3876543434343"
+    assert candidate_info.city == "New York"
+    assert candidate_info.street_address == "Broadway"
+    #
+    # Experience Information
+    # 
+    assert candidate_info.candidate_skill == "Housekeep"
+    assert candidate_info.candidate_experience == "One Year +"
+    assert candidate_info.candidate_experience == "Two Year"
+    assert candidate_info.candidate_training == "Self Taught"
+    #
+    # Additional Information
+    #
+    assert candidate_info.work_hours == "30+"
+    assert candidate_info.payed_per_hour == "20.0"
+    assert candidate_info.valid_work_permit == "No"
+    assert candidate_info.drivers_license == "B"
