33ff21cc7a647c94092b7a3b041e1655b605bc45
Copser <ppilipovic84@gmail.com>
2016-12-14 05:29:53 +0100

    reder are view using generic FormView

--- superjobs/candidate_form/views.py
+++ superjobs/candidate_form/views.py
@@ -3,37 +3,14 @@
 from __future__ import unicode_literals
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.shortcuts import render_to_response, redirect
-from django.http import HttpResponse, HttpResponseRedirect
-from django.template import Context, loader, RequestContext
+from django.views.generic import FormView
 
 from candidate_form.forms import CandidateForm
 
 # Create your views here.
-# @login_required
-def apply_for_job(request):
-    if request.method == "POST":
-        form = CandidateForm(
-            data=request.POST,
-            files=request.FILES,
-        )
-        if form.is_valid():
-            form = form.save(commit=False)
-            form.save()
-            messages.add_message(
-                request, messages.INFO, "You have applied, Thank you!"
-            )
-            return HttpResponseRedirect("/success")
-        else:
-            print(form.errors)
-    else:
-        form = CandidateForm()
-
-    t = loader.get_template('candidate_apply.html')
-    c = RequestContext(
-        request,
-        {
-            'form': form
-        }
-    )
-    return HttpResponse(t.render(c))
+class ApplyFormView(FormView):
+    """TODO: Render CandidateForm which will inherit from FormView
+    return: TODO
+    """
+    tempate_name = 'candidate_form/candidate_apply.html'
+    form_class = CandidateForm
