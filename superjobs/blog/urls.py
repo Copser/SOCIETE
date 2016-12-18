207dda88fe905dfc39d09bfcf0ff7e07789492f1
Copser <ppilipovic84@gmail.com>
2016-12-18 07:15:05 +0100

    remove success url

--- superjobs/blog/urls.py
+++ superjobs/blog/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
 
     url(r'^jobs/$', views.jobs, name='jobs'),
     url(r'^apply_to/$', views.apply_to, name='apply_to'),
-    url(r'^success/$', views.success, name='success'),
     url(r'^(?P<slug>[\w|\-]+)/$', views.post, name='post'),
 ]
 
