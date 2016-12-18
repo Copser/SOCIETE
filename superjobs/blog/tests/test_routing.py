deca61e871ddf53af33b977420d1eb64d96a1c9f
Copser <ppilipovic84@gmail.com>
2016-12-18 22:22:52 +0100

    write routing tests to see if are url are working correctly

--- superjobs/blog/tests/test_routing.py
+++ superjobs/blog/tests/test_routing.py
@@ -16,35 +16,15 @@ class JobsPageTest(TestCase):
     wired correctly
     return: TODO
     """
-    def test_jobs_resolvers_to_jobs_view(self):
-        """TODO: should return jobs page
-        return: TODO
-        """
+
+    def test_jobs_resolvers_to_blog_jobs(self):
         jobs_page = resolve('/blog/jobs/')
-        jobs_page_status_code = self.client.get('/blog/jobs/')
         assert jobs_page.func == jobs
-        assert jobs_page_status_code.status_code == 200
 
-    def test_post_list_resolvers_to_post_list_api_view(self):
-        """TODO: testing posts_list url routing, should return posts_list
-        return: TODO
-        """
-        posts_list_jobs_api = resolve('/blog/posts/')
-        posts_lists_status_code = self.client.get('/blog/posts/')
-        assert posts_list_jobs_api.func == posts_list
-        assert posts_lists_status_code.status_code == 200
+    def test_apply_to_resolvers_to_blog_apply_to(self):
+        blog_apply_to = resolve('/blog/apply_to/')
+        assert blog_apply_to.func == apply_to
 
-    def test_post_detail_resolvers_to_post_deatail_api_view(self):
-        """TODO: testing posts_detail url api routing, should return posts_detail
-        return: TODO
-        """
-        posts_detail_api = resolve('/blog/posts/1/')
-        assert posts_detail_api.func == posts_detail
-
-
-    def test_apply_to_return_proper_status_code(self):
-        """TODO: test apply_to status code return 200
-        return: TODO
-        """
-        apply_to_status_code = self.client.get('/blog/apply_to/')
-        assert apply_to_status_code.status_code == 200
+    def test_blog_posts_resolvers_to_blog_posts_api(self):
+        blog_posts = resolve('/blog/posts/')
+        assert blog_posts.func == posts_list
