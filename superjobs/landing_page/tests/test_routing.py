906f15efb4d0410e1efd5fceb826bec9e5fab3d1
Copser <ppilipovic84@gmail.com>
2016-12-18 09:21:13 +0100

    refactor tests

--- superjobs/landing_page/tests/test_routing.py
+++ superjobs/landing_page/tests/test_routing.py
@@ -1,18 +1,22 @@
+# landing_page/tests/test_routing
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
 import pytest
-from django.test import TestCase
 
-from django.core.urlresolvers import resolve
-from django.shortcuts import render_to_response
 
 from landing_page.views import index, about, success
-
 
 def test_index_page_status_code(rf):
     request = rf.get('/')
     response = index(request)
     assert response.status_code == 200
 
-def test_about_page_statis_code(rf):
+def test_about_page_status_code(rf):
     request = rf.get('/about')
     response = about(request)
+    assert response.status_code == 200
+
+def test_success_page_status_code(rf):
+    request = rf.get('/success')
+    response = success(request)
     assert response.status_code == 200
