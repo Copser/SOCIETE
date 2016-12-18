feb856c5efaf0506cd6456f708cb335ab652357a
Copser <ppilipovic84@gmail.com>
2016-12-18 06:39:15 +0100

    modify form tests

--- superjobs/blog/tests/test_forms.py
+++ superjobs/blog/tests/test_forms.py
@@ -1,11 +1,39 @@
 # blog/tests/test_forms.py
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals
+from django import forms
 
 import unittest
 import pytest
 
 from blog.forms import ApplyForm
+
+
+
+def test_user_form_fields_match():
+    form_fields = ApplyForm(
+        {
+            "first_name" : "John",
+            "last_name" : "Doe",
+            "mobile_phone" : "+11223344556677",
+            "confirm_mobile_phone" : "+11223344556677",
+            "city" : "Manhattan",
+            "street_address" : "",
+
+            "candidate_skill" : "Carpenter",
+            "candidate_experience": "One Year",
+            "candidate_hospitality_experience": "Two Years",
+            "candidate_training": "Self Taught",
+
+            "work_hours": "10+",
+            "payed_per_hour": "20.00",
+            "valid_work_permit": "Yes",
+            "drivers_license": "A"
+        }
+    )
+
+    for form_field in form_fields:
+        assert form_field.is_valid()
 
 
 class FormTesterMixin():
