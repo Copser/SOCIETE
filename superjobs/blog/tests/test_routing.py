006b53c23017fa0ec5607ab24ea861b6df29922f
Copser <ppilipovic84@gmail.com>
2016-12-19 19:48:40 +0100

    remove api view tests

--- superjobs/blog/tests/test_routing.py
+++ superjobs/blog/tests/test_routing.py
@@ -11,20 +11,18 @@ from blog.views import jobs, posts_list, posts_detail, apply_to
 import pytest
 
 
-class JobsPageTest(TestCase):
-    """TODO: testing blog/ routing, we want to see if are pages are
-    wired correctly
-    return: TODO
-    """
+def test_jobs_resolvers_to_blog_jobs():
+    jobs_page = resolve("/blog/jobs/")
+    assert jobs_page.func == jobs
 
-    def test_jobs_resolvers_to_blog_jobs(self):
-        jobs_page = resolve('/blog/jobs/')
-        assert jobs_page.func == jobs
+def test_blog_jobs_returns_appropriate_html(client):
+    jobs = client.get("/blog/jobs/")
+    assert jobs.status_code == 302, "Tests will it redirect to login_url"
 
-    def test_apply_to_resolvers_to_blog_apply_to(self):
-        blog_apply_to = resolve('/blog/apply_to/')
-        assert blog_apply_to.func == apply_to
+def test_apply_to_resolvers_to_blog_apply_to():
+    blog_apply_to = resolve('/blog/apply_to/')
+    assert blog_apply_to.func == apply_to
 
-    def test_blog_posts_resolvers_to_blog_posts_api(self):
-        blog_posts = resolve('/blog/posts/')
-        assert blog_posts.func == posts_list
+def test_blog_apply_to_returns_appropriate_html(client):
+    apply_to = client.get("/blog/apply_to/")
+    assert apply_to.status_code == 302, "Tests will it redirect to login_url"
