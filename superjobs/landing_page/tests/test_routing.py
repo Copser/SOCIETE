75a7bd0bef25c9c02bd99afcf85ce32acabb338b
Copser <ppilipovic84@gmail.com>
2016-12-20 21:25:54 +0100

    add django testcase class

--- superjobs/landing_page/tests/test_routing.py
+++ superjobs/landing_page/tests/test_routing.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 from __future__ import unicode_literals
 
+from django.test import TestCase
 from django.core.urlresolvers import resolve, reverse
 from django.shortcuts import render_to_response
 
@@ -10,29 +11,31 @@ from landing_page.views import index, about, success
 import pytest
 
 
-def test_index_resolvers_to_main_page():
-    main_page = resolve("/")
-    assert main_page.func == index
+class MainPageTests(TestCase):
+    """TODO: Testing main page, about page, and success page
+    ruting, status_code
+    return: TODO
+    """
+    def test_index_resolvers_to_main_page(self):
+        main_page = resolve("/")
+        assert main_page.func == index
 
-def test_index_page_status_code(rf):
-    request = rf.get('/')
-    response = index(request)
-    assert response.status_code == 200
+    def test_index_page_status_code(self):
+        index = self.client.get('/')
+        assert index.status_code == 200
 
-def test_about_page_resolvers_to_about_page():
-    about_page = resolve('/about/')
-    assert about_page.func == about
+    def test_about_page_resolvers_to_about_page(self):
+        about_page = resolve('/about/')
+        assert about_page.func == about
 
-def test_about_page_status_code(rf):
-    request = rf.get('/about')
-    response = about(request)
-    assert response.status_code == 200
+    def test_about_page_status_code(self):
+        about = self.client.get('/about/')
+        assert about.status_code == 200
 
-def test_success_page_resolvers_to_success_page():
-    success_page = resolve("/success/")
-    assert success_page.func == success
+    def test_success_page_resolvers_to_success_page(self):
+        success_page = resolve("/success/")
+        assert success_page.func == success
 
-def test_success_page_status_code(rf):
-    request = rf.get('/success')
-    response = success(request)
-    assert response.status_code == 200
+    def test_success_page_status_code(self):
+        success = self.client.get('/success/')
+        assert success.status_code == 200
