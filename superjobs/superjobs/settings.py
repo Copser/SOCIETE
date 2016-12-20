4102248ab265b52a4c3897403bb1da0b06d1dd58
Copser <ppilipovic84@gmail.com>
2016-12-20 21:27:58 +0100

    fix typo

--- superjobs/superjobs/settings.py
+++ superjobs/superjobs/settings.py
@@ -83,7 +83,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
-            os.path.join(BASE_DIR, 'templates', 'candidate_form'),
+            os.path.join(BASE_DIR, 'templates', 'landing_page'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
