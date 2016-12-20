c4fa1b3c73c53c6475e5ddcf5f1b04c249ebf15a
Copser <ppilipovic84@gmail.com>
2016-12-20 21:20:06 +0100

    adding django TestCase class

--- superjobs/blog/tests/test_routing.py
+++ superjobs/blog/tests/test_routing.py
@@ -10,19 +10,19 @@ from blog.views import jobs, posts_list, posts_detail, apply_to
 
 import pytest
 
+class BlogTests(TestCase):
+    def test_jobs_resolvers_to_blog_jobs(self):
+        jobs_page = resolve("/blog/jobs/")
+        assert jobs_page.func == jobs
 
-def test_jobs_resolvers_to_blog_jobs():
-    jobs_page = resolve("/blog/jobs/")
-    assert jobs_page.func == jobs
+    def test_blog_jobs_returns_appropriate_html(self):
+        jobs = self.client.get("/blog/jobs/")
+        assert jobs.status_code == 302, "Tests will it redirect to login_url"
 
-def test_blog_jobs_returns_appropriate_html(client):
-    jobs = client.get("/blog/jobs/")
-    assert jobs.status_code == 302, "Tests will it redirect to login_url"
+    def test_apply_to_resolvers_to_blog_apply_to(self):
+        blog_apply_to = resolve('/blog/apply_to/')
+        assert blog_apply_to.func == apply_to
 
-def test_apply_to_resolvers_to_blog_apply_to():
-    blog_apply_to = resolve('/blog/apply_to/')
-    assert blog_apply_to.func == apply_to
-
-def test_blog_apply_to_returns_appropriate_html(client):
-    apply_to = client.get("/blog/apply_to/")
-    assert apply_to.status_code == 302, "Tests will it redirect to login_url"
+    def test_blog_apply_to_returns_appropriate_html(self):
+        apply_to = self.client.get("/blog/apply_to/")
+        assert apply_to.status_code == 302, "Tests will it redirect to login_url"
