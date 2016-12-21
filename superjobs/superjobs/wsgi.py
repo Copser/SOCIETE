d537d4c79ba93fe1678905ba8d4df060a036ea56
Copser <ppilipovic84@gmail.com>
2016-12-21 21:36:07 +0100

    adding memcachier heroku configuration

--- superjobs/superjobs/wsgi.py
+++ superjobs/superjobs/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 from dj_static import Cling, MediaCling
 from django.core.wsgi import get_wsgi_application
+from django.core.cache.backends.memcached import BaseMemcachedCache
+
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superjobs.settings")
-
+# Fix django closing connection to MemCachier after every request
+BaseMemcachedCache.close = lambda self, **kwargs: None
 application = Cling(MediaCling(get_wsgi_application()))
