a4f6180b05a5123c1b1c163fc8c71c512871819d
Copser <ppilipovic84@gmail.com>
2016-12-23 20:44:15 +0100

    adding custom viewset for post

--- superjobs/blog/api/views.py
+++ superjobs/blog/api/views.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 from rest_framework import generics
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
 from blog.models import Post
 from .serializers import PostSerializer
@@ -24,6 +25,15 @@ class PostListView(generics.ListAPIView):
 class PostDetailView(generics.RetrieveAPIView):
     """TODO: Define api detailed view for the Post model,
     using rest_framework generics.RetrieveAPIView
+    return: TODO
+    """
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostViewSet(viewsets.ReadOnlyModelViewSet):
+    """TODO: Define viewsets for Post, we will have better
+    inrecation in our API and we let REST build the URLs dynamically
     return: TODO
     """
     queryset = Post.objects.all()
