912d20562d33b21b5570b6d9bf31264f1cb6c0b2
Copser <ppilipovic84@gmail.com>
2017-01-11 20:18:23 +0100

    remove contact app

--- societe/contact/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from contact import views
-
-
-urlpatterns = [
-    url(r'^contact/$', views.contact, name="contact"),
-    url(r'^success/$', views.success, name="success"),
-]
