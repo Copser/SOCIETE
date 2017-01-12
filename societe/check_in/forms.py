8a977220b8e5113bdfad2e118b9b9937f3562388
Copser <ppilipovic84@gmail.com>
2017-01-12 05:53:58 +0100

    building new arange form

---
+++ societe/check_in/forms.py
@@ -0,0 +1,56 @@
+# check_in/forms.py
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _,\
+        ugettext
+from django import forms
+
+from crispy_forms.helper import FormHelper
+from crispy_forms import layout, bootstrap
+from crispy_forms.bootstrap import InlineField
+
+from check_in.models import InitialCharacteristicModel
+
+
+class InitialCharacteristicForm(forms.ModelForm):
+    """TODO: Custom form for InitialCharacteristicModel,
+    I'm using django crispy_forms to utilaze and customize it
+    return: TODO
+    """
+    def __init__(self, *args, **kwargs):
+        super(InitialCharacteristicForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.method = "POST"
+        self.helper.form_action = ""
+
+        self.helper.layout = layout.Layout(
+            layout.Fieldset(
+                _("Personal Information"),
+                layout.Field(
+                    "first_name",
+                    css_class="input-block-level",
+                    placeholder="First Name"
+                ),
+                layout.Field(
+                    "last_name",
+                    css_class="input-block-level",
+                    placeholder="Last Name",
+                ),
+                layout.Field(
+                    bootstrap.PrependText("email", "@",
+                                          css_class="input-block-level",
+                                          placeholder="youremail@example.com"),
+                ),
+                bootstrap.FormActions(
+                    layout.Submit("submit", _("Submit")),
+                )
+            )
+        )
+
+        class Meta:
+            models = InitialCharacteristicModel
+            fields=[ "first_name", "last_name", "email", "country",
+                    "move_in_date", "move_out_date", "message"]
+
